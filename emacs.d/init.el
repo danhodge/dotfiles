9b3024b4a352b2dd90f28475969371ac638ca68a
danhodge <dan@swipely.com>
2014-08-04 13:26:24 -0400

    why hello, emacs!

---
+++ emacs.d/init.el
@@ -0,0 +1,9 @@
+;; http://stackoverflow.com/questions/19524627/where-is-my-emacs-file-on-mac-os-x
+
+(setq ipackage-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+;; show cursor position
+(column-number-mode 1)
+
