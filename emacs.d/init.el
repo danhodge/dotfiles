0f8117a84c7fd4b2a72e5a96cba432e8ceb8cd79
Dan Hodge <danhodge@gmail.com>
2014-08-12 22:20:49 -0400

    fixed typo

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,8 +1,10 @@
 ;; http://stackoverflow.com/questions/19524627/where-is-my-emacs-file-on-mac-os-x
 
-(setq ipackage-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+(package-refresh-contents)
 
 ;; show cursor position
 (column-number-mode 1)
