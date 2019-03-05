bb85dd3f7fe079dce9d3810a2bdf3e60d5ae5444
danhodge <danhodge@upserve.com>
2019-03-05 13:35:33 -0500

    add ace-window

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -21,6 +21,7 @@
 ; Additional config
 (require 'init-theme)
 
+(require 'init-ace-window)
 (require 'init-helm)
 (require 'init-magit)
 (require 'init-projectile)
---
+++ emacs.d/init/init-ace-window.el
@@ -0,0 +1,12 @@
+;;; init-ace-window.el --- ace window configuration
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package ace-window
+  :ensure t
+  :bind (("M-o" . ace-window)))
+
+(provide 'init-ace-window)
+;;; init-ace-window.el ends here
