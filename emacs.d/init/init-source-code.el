6a3704d57f2a1878d4669bbbfda1fd1e66811900
danhodge <danhodge@upserve.com>
2019-03-07 14:47:33 -0500

    some more source code packages & config
    * add smartparens
    * add company (auto-completion)
    * enable subword minor mode for subword navigation when editing source code

--- emacs.d/init/init-source-code.el
+++ emacs.d/init/init-source-code.el
@@ -14,13 +14,30 @@
   :init
   (setq fci-rule-column 100))
 
+
+(use-package smartparens
+  :ensure t
+  :config
+  (require 'smartparens-config))
+
+(use-package company
+  :ensure t
+  :config
+  (setq company-idle-delay 0.5)
+  (setq company-tooltip-limit 10)
+  (setq company-minimum-prefix-length 2)
+  (setq company-tooltip-flip-when-above t)
+  (global-company-mode))
+
 (defun dh/prog-hooks ()
   "general programming language settings"
   (whitespace-mode)
   (linum-mode 1)
-  (fci-mode 1))
+  (fci-mode 1)
+  (subword-mode +1))
 
 (add-hook 'prog-mode-hook #'dh/prog-hooks)
+(add-hook 'prog-mode-hook  #'smartparens-mode)
 
 (provide 'init-source-code)
 ;;; init-source-code.el ends here
