b0f117cdf15672eb8f0eea069b599670af3b8173
danhodge <danhodge@upserve.com>
2019-03-07 14:45:01 -0500

    update helm configuration
    add the C-x C-b and C-x C-f key bindings used by prelude and set some
    variables (also from prelude) to make helm fuzzy find work the same
    way it does in prelude

--- emacs.d/init/init-helm.el
+++ emacs.d/init/init-helm.el
@@ -6,7 +6,15 @@
 
 (use-package helm
   :ensure t
-  :bind (("M-y" . helm-show-kill-ring)))
+  :init
+  (setq helm-split-window-in-side-p t)
+  (setq helm-buffers-fuzzy-matching t)
+  (setq helm-move-to-line-cycle-in-source t)
+  (setq helm-ff-search-library-in-sexp t)
+  (setq helm-ff-file-name-history-use-recentf t)
+  :bind (("M-y" . helm-show-kill-ring)
+         ("C-x C-b" . helm-buffers-list)
+         ("C-x C-f" . helm-find-files)))
 
 (provide 'init-helm)
 ;;; init-helm.el ends here
